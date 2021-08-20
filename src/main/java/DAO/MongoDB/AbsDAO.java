1d74e75215cfe94fc1fc6c5f5a056877d72f886e
noon111101 <trieu11112001@gmail.com>
2021-08-20 15:51:53 +0700

    Web1

--- src/main/java/DAO/MongoDB/AbsDAO.java
+++ src/main/java/DAO/MongoDB/AbsDAO.java
@@ -16,7 +16,7 @@ public abstract class AbsDAO {
 
     MongoDatabase getDB() {
         if (db == null) {
-            //ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017");
+
             ConnectionString connectionString = new ConnectionString("mongodb+srv://root:root@cluster0.lh5rj.mongodb.net");
             MongoClientSettings settings = MongoClientSettings.builder()
                     .applyConnectionString(connectionString)
